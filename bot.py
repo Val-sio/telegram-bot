d4b8e392c637fd89b0ea2167b41d3e0caf7e92cc
Val Sio <4488val@gmail.com>
2026-05-22 15:49:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ from telegram.ext import (
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8610613129:AAGtDbgtyxigBi3E_BaRb7R6DpI-HQvh3cc")
 ADMIN_CHAT_ID = 1150947024
-OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY", "")
+OPENROUTER_API_KEY = "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"", "")
 CSV_FILE = "orders.csv"
 
 SYSTEM_PROMPT = """Ты умный помощник в Telegram-боте.
