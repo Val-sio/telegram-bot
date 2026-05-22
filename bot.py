4eb2e2cbbecaf003ccf7b98b1ea6c208e6436961
Val Sio <4488val@gmail.com>
2026-05-22 10:13:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,9 +19,9 @@ from telegram.ext import (
 )
 
 # ─── Настройки ───────────────────────────────────────────────────────────────
-BOT_TOKEN =os.environ.get("BOT_TOKEN", "8610613129:AAGtDbgtyxigBi3E_BaRb7R6DpI-HQvh3cc")       # токен от @BotFa
+BOT_TOKEN = "8610613129:AAGtDbgtyxigBi3E_BaRb7R6DpI-HQvh3cc" # токен от @BotFa
 ADMIN_CHAT_ID = 1150947024           
-OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY" , "")
+OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY", "")
 CSV_FILE = "orders.csv"
 
 SYSTEM_PROMPT = """Ты умный помощник в Telegram-боте.
